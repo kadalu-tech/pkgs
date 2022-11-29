af4142b43e3280aa615543b386b23204a1f8488f
Aravinda Vishwanathapura <aravinda@kadalu.tech>
2022-11-29 19:08:18 +0530

    Build packages for a tag (#14)
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.tech>

---
+++ runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+version=$1
+dist=$2
+major_version=$(cut -d "." -f 1 <<< $version)
+minor_version=$(cut -d "." -f 2 <<< $version)
+
+FILE=src/${major_version}.${minor_version}.x/${dist}.t
+if [ -f "$FILE" ]; then
+    echo "Version: ${version} Runner: ${FILE}"
+    PKG_VERSION=${version} PACKAGING_GPG_SIGNING_KEY=${PACKAGING_GPG_SIGNING_KEY} \
+               binnacle -v ${FILE}
+else
+    echo "No runner available for ${version} ${dist}"
+fi
